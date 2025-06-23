d928db6d768b044ee0f70f8cb4413bc44beab05d
Vitaly Zdanevich <zdanevich.vitaly@ya.ru>
2025-06-24 02:12:05 +0400

    search.go documentation: simplify code example

--- src/sort/search.go
+++ src/sort/search.go
@@ -51,7 +51,7 @@ package sort
 //		answer := sort.Search(100, func(i int) bool {
 //			fmt.Printf("Is your number <= %d? ", i)
 //			fmt.Scanf("%s", &s)
-//			return s != "" && s[0] == 'y'
+//			return s[0] == 'y'
 //		})
 //		fmt.Printf("Your number is %d.\n", answer)
 //	}
